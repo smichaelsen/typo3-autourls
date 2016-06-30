46ec4e2ce9a86db135d4deec82c39b0b6b522e56
Sebastian Michaelsen <sebastian@michaelsen.io>
2016-06-30 07:58:45 +0200

    [TASK] whitespace

--- Classes/ExtensionParameterRegistry.php
+++ Classes/ExtensionParameterRegistry.php
@@ -13,7 +13,7 @@ namespace Smichaelsen\Autourls;
  * * * The first one obviously is variable. It's not always 42. It is a uid that we want to be replaced with the title of a database record. Therefore we replace it with _UID_
  * * * The second and third ones are static. Out route makes only sense if these two are set to News and detail. We keep them in the query string like that. They will disappear from the generated url path.
  * * The 3rd method argument is a table name that needs to be provided if our query string contains a _UID_. The TCA label configuration will be used to get a title for the database record.
- * * Our example result is: \Smichaelsen\Autourls\ExtensionParameterRegistry::register('News','tx_news_pi1[news]=_UID_&tx_news_pi1[controller]=News&tx_news_pi1[action]=detail','tx_news_domain_model_news');
+ * * Our example result is: \Smichaelsen\Autourls\ExtensionParameterRegistry::register('News', 'tx_news_pi1[news]=_UID_&tx_news_pi1[controller]=News&tx_news_pi1[action]=detail', 'tx_news_domain_model_news');
  * * The resulting url could look like: www.example.com/path/to/news-detail/news/the-title-of-my-news
  *
  * * You can also use _PASS_ as value in your query string. If we set tx_news_pi1[news]=_PASS_ in our example we get an url like: www.example.com/path/to/news-detail/news/42
