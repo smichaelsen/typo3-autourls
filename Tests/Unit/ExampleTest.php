6756f85dc69cc291fd52765d8e387b6a922d32be
Sebastian Michaelsen <sebastian@michaelsen.io>
2016-12-07 16:59:04 +0100

    [TASK] Set up unit testing
    Run tests: .Build/bin/phpunit -c UnitTests.xml

---
+++ Tests/Unit/ExampleTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace Smichaelsen\Autourls\Tests\Unit;
+
+use TYPO3\CMS\Core\Tests\UnitTestCase;
+
+class ExampleTest extends UnitTestCase
+{
+
+    /**
+     * @test
+     */
+    public function example()
+    {
+        $this->assertTrue(true);
+    }
+
+}
